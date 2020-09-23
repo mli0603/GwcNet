d75d273e6ee94b4f9ca69c79a2935349b5ae9290
Max <maxwell.lzshuo@gmail.com>
2020-09-23 17:37:58 -0400

    added midd dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,7 +1,9 @@
 from .kitti_dataset import KITTIDataset
 from .sceneflow_dataset import SceneFlowDatset
+from .middlebury_dataset import MiddleburyDatset
 
 __datasets__ = {
     "sceneflow": SceneFlowDatset,
-    "kitti": KITTIDataset
+    "kitti": KITTIDataset,
+    "middlebury": MiddleburyDatset
 }
